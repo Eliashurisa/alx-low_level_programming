6bf96cf7a8736024c5aab8de31d13e7e9b759757
Elias Girma <eliashurisa@gmail.com>
2022-10-16 17:03:39 +0300

    use printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Print a string
+ * Description: use printf
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+Footer
+© 2022 GitHub, 
