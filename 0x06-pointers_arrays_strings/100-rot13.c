3226eeaa532dcf512cd18b0b24a077bbb5d176cf
Elias Girma <eliashurisa@gmail.com>
2022-10-27 21:48:14 +0300

    test

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,25 +1,45 @@
 #include "main.h"
 
 /**
-  * print_number - Prints any integer with putchar
-  * @n: Number to prints
-  *
-  * Return: Nothing
-  */
-void print_number(int n)
+* rot13 -  encodes a string using rot13
+* @str:the string targeted
+*Return: returns the encoded string
+*/
+
+char *rot13(char *str)
 {
-	unsigned int x;
+	int index1, index2;
 
-	if (n < 0)
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+											 'G', 'H', 'I', 'J', 'K', 'L',
+											 'M', 'N', 'O', 'P', 'Q', 'R',
+											 'S', 'T', 'U', 'V', 'W', 'X',
+											 'Y', 'Z', 'a', 'b', 'c', 'd',
+											 'e', 'f', 'g', 'h', 'i', 'j',
+											 'k', 'l', 'm', 'n', 'o', 'p',
+											 'q', 'r', 's', 't', 'u', 'v',
+											 'w', 'x', 'y', 'z'};
+
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+											 'T', 'U', 'V', 'W', 'X', 'Y',
+											 'Z', 'A', 'B', 'C', 'D', 'E',
+											 'F', 'G', 'H', 'I', 'J', 'K',
+											 'L', 'M', 'n', 'o', 'p', 'q',
+											 'r', 's', 't', 'u', 'v', 'w',
+											 'x', 'y', 'z', 'a', 'b', 'c',
+											 'd', 'e', 'f', 'g', 'h', 'i',
+											 'j', 'k', 'l', 'm'};
+	while (str[++index1])
 	{
-		_putchar('-');
-		n *= -1;
+		for (index2 = 0; index2 < 52; index2++)
+		{
+			if (str[index1] == alphabet[index2])
+			{
+				str[index1] = rot13key[index2];
+				break;
+			}
+		}
 	}
-
-	x = n;
-
-	if (x / 10)
-		print_number(x / 10);
-
-	_putchar(x % 10 + '0');
+	return (str);
 }
+
