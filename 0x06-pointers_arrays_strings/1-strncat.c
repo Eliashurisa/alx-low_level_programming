db151ca06fd9e55da2190f992e428e93883302d4
Elias Girma <eliashurisa@gmail.com>
2022-10-27 21:26:40 +0300

    for test

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int dlen = 0, j = 0;
+
+	while (dest[dlen])
+	{
+		dlen++;
+	}
+
+	while (j < n && src[j])
+	{
+		dest[dlen] = src[j];
+		dlen++;
+		j++;
+	}
+
+	dest[dlen + n + 1] = '\0';
+
+	return (dest);
+}
+i
